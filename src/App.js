76bb2d61ddd7dcf8aa93e94d6757cb9b6fc2fe86
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2022-10-11 14:25:11 -0400

    got header and nav set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import "./App.css";
+function App() {
+  return (
+    <header className="react-app">
+      <Nav />
+      <Header />
+    </header>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function Nav() {
+  const linkStyle = { border: "25px black", padding: "35px" };
+
+  return (
+    <nav>
+      <section
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "flex-end",
+          justifyContent: "flex-end",
+        }}
+      >
+        <div style={linkStyle}>
+          <a className="nav" href="#">
+            About Me
+          </a>
+        </div>
+        <div style={linkStyle}>
+          <a className="nav" href="#">
+            Portfolio
+          </a>
+        </div>
+        <div style={linkStyle}>
+          <a className="nav" href="#">
+            Contact
+          </a>
+        </div>
+        <div style={linkStyle}>
+          <a className="nav" href="#">
+            Resume
+          </a>
+        </div>
+      </section>
+    </nav>
+  );
+}
